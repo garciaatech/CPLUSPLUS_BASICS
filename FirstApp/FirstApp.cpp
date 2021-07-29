e207a6bfd09719a4dee13fc580ea008982c1ecb5
AA Garcia <80655952+garciaatech@users.noreply.github.com>
2021-07-29 17:12:46 -0400

    Add files via upload

---
+++ FirstApp/FirstApp.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //header file/library //allows for inputting and outputting data //this allows cout
+using namespace std; //reference std as a global variable to not reference anymore // removing the std::cout <<
+
+int main() //main function/main procedure //will execute what is within
+{
+    cout << "Hello World!\n"; //console output //:: redirection sign
+    cout << "Learning C++ again.\n";
+    cout << "Learning C++ again. Twice.";
+    system("pause>0"); //gets rid of fluff at end of console
+}
+
+//MUST BUILD CODE TO TRANSFER FROM C++ TO COMPUTER UNDERSTOOD LANGUAGE
+//BUILD MUST HAPPEN TO TRANSLATE
---
+++ FourthApp/FourthApp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int intMax = INT_MAX;
+
+    cout << intMax << endl;
+
+    cout << intMax + 1; //Data Type Overflow // Restarts from lowest number when overflow occurs
+
+    system("pause>0");
+}
